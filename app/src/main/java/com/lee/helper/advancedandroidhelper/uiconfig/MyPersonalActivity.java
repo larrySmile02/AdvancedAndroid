366db7e9b30f9d110074f0e6c85d11673f10d358
李越 <liyue@tuya.com>
2019-01-27 00:34:23 +0800

    初次使用Rxjava + okhttp获取数据及处理数据

--- app/src/main/java/com/lee/helper/advancedandroidhelper/uiconfig/MyPersonalActivity.java
+++ app/src/main/java/com/lee/helper/advancedandroidhelper/uiconfig/MyPersonalActivity.java
@@ -1,14 +1,29 @@
 package com.lee.helper.advancedandroidhelper.uiconfig;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 
 import com.lee.helper.advancedandroidhelper.R;
 import com.lee.helper.config.ConfigUIActivity;
+import com.lee.helper.smartokhttp.HttpUtils;
+import com.lee.helper.toast.ToastUtils;
+
+import java.io.IOException;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.Call;
+import okhttp3.Response;
 
 
-public class MyPersonalActivity extends ConfigUIActivity
-{
+public class MyPersonalActivity extends ConfigUIActivity {
+
+    private static final String GRID_API_UK = "https://api.backendless.com/DBB73A3F-4B87-5C72-FF30-BCE8810F8700/3F81588C-4200-BA2F-FF66-2FDE2DF63400/services/GridSettingsUK/GetSettings";
 
     @Override
     public int getConfigBase() {
@@ -19,17 +34,64 @@ public class MyPersonalActivity extends ConfigUIActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personalcenter_fragment_native);
+        initData();
 
 
+    }
 
+    private void initData() {
+        Observable.create(new ObservableOnSubscribe<String>() {
+
+            @Override
+            public void subscribe(final ObservableEmitter<String> emitter) {
+
+                HttpUtils.get(GRID_API_UK, new okhttp3.Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                        emitter.onNext(e.toString());
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) {
+                        if(response.isSuccessful()){
+                            emitter.onNext(response.body().toString());
+                        }
+                    }
+                });
+
+
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String sb) {
+                        ToastUtils.show(sb);
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
-
-
 
 
 }
---
+++ smartokhttp/src/main/java/com/lee/helper/smartokhttp/HttpUtils.java
@@ -0,0 +1,21 @@
+package com.lee.helper.smartokhttp;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class HttpUtils
+{
+    public static void get(String url , Callback callback){
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().
+                url(url).
+                build();
+        client.newCall(request).enqueue(callback);
+    }
+}
